05f579ff365d1bee4df028b80b0d6e0f0800764c
joel <joel.wenzel@flexagon.com>
2020-02-21 11:15:39 -0600

    BLOG-16 another thing

--- XXHR/java/xxt/oracle/apps/ak/xxperson/webui/xxPersonMainCO.java
+++ XXHR/java/xxt/oracle/apps/ak/xxperson/webui/xxPersonMainCO.java
@@ -5,7 +5,7 @@
  |  HISTORY                                                                  |
  +===========================================================================*/
 package xxt.oracle.apps.ak.xxperson.webui;
-//balch
+//balch123
 import oracle.apps.fnd.common.VersionInfo;
 import oracle.apps.fnd.framework.webui.OAControllerImpl;
 import oracle.apps.fnd.framework.webui.OAPageContext;
